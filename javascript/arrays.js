3ffcc255685e234d28e8fef204f3eb683a924440
Iván Sánchez <ivan.ravecca@gmail.com>
2021-01-15 19:54:57 -0300

    Update arrays.js

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -19,3 +19,30 @@ function deleteNth(arr,n){
   }
   return output;
 }
+
+/*
+Given an unsorted array or numbers, return it sorted but even numbers should remain in the same position on the original
+*/
+function sortArray(array) {
+  const odds = array.filter((number) => number % 2).sort((a, b) => a - b);
+  return array.map((number) => number % 2 ? odds.shift() : number);
+}
+
+/* 
+We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) 
+and outputs a list of the sorted numbers in the range that fulfills the property described above.
+*/
+function sumDigPow(a, b) {
+  var out = [];
+  for(var i = a; i <= b; i++) {
+    if (i > 0) {
+      var sum = Array.from(i.toString()).map(Number).reduce((acc, curr, index) => {
+        return acc + Math.pow(curr, index + 1);
+      }, 0);
+      if (sum === i) {
+          out.push(i);
+      }
+    }
+  }
+  return out;
+}
